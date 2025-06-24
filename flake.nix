d658166850375ec76d31ffab91197f29752bc191
YoussefDevPro <youssef.pro.coding@gmail.com>
2025-06-24 17:19:12 +0100

    removed flake, and made the code prototype

--- flake.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-let
-  rustPkgs = pkgs.callPackage ./Cargo.nix { };
-in
-rustPkgs.rootCrate.build
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+use std::net::TcpStream;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Message {
+    pub sender: u64,
+    pub server: u64,
+    pub channel: u64,
+    pub content: String,
+    pub is_replying: bool,
+    pub replied_to_msg_id: u64,
 }
+// welll, this is a data prototype of a message, im gonna make a tui until the server side is done,
+// then ill make some function to make things much easier, after ill make the tui, and here things
+// will start to be a lil challenging
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:6969").expect("Could not connect to server");
+
+    let username = 123456789;
+    let msg = Message {
+        sender: username.clone(),
+        server: 1,
+        channel: 1,
+        content: "Hello!".to_string(),
+        is_replying: false,
+        replied_to_msg_id: 0,
+    };
+
+    let json = serde_json::to_vec(&msg).expect("Failed to serialize");
+    stream.write_all(&json).expect("Failed to send message");
+} // simple connection with the server in a nice-nice localhost, make a test message then send it
+  // to the nice-nice server, just to see if things work
